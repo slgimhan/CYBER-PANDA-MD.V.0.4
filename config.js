844f104f44b877bf51e2c3813776790d8a628789
slgimhan <167409370+slgimhan@users.noreply.github.com>
2024-04-27 11:30:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ const chalk = require("chalk");
 
 //
 global.available = true;
-global.autoReadAll = false;
+global.autoReadAll = true;
 global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
+global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = true;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = false;                 //This is the new variable for controlling status seen.
@@ -21,13 +21,13 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.Owner = ["+94713317339", "94713317339"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94713317339", "+94713317339"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94713317339"];
+global.OwnerName = "𝙶𝙸𝙼 | 𝙱𝙰𝙱𝙰𝙷:ヤ";
+global.BotName = "𝙶𝙸𝙼 | 𝙱𝙰𝙱𝙰𝙷:(⁠ᵔ⁠ᴥ⁠ᵔ⁠)";
+global.packname = "🦋 𝙽𝙾|𝙾𝙱:ಥ⁠_⁠ಥ 🦋";                             //Do not change.
+global.author = "𝙽𝙾|𝙾𝙱:ヤ";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
@@ -41,7 +41,7 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "mathara, Sri Lankan";
+global.location = "rabukkana, Sri Lankan";
 global.reactmoji = "💞";
 global.themeemoji = "😂";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
